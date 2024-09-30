ab24a38e12d62344884c35600e55a33aa4af7a48
M.Jahanzaib Abid <150152743+Fahad16232@users.noreply.github.com>
2024-10-01 01:26:51 +0500

    Initial Commit

---
+++ Clock Master/Clock_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_MasterApp.swift
+//  Clock Master
+//
+//  Created by Fahad on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Clock_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
